67d93c86dc8ea42fb53e95358cbf6de0b7e9be8e
Joonas Suonpera <jsuonper@student.hive.fi>
2020-03-12 11:22:02 +0200

    norme passed. leaks ok. EVERYTHING OK HERE

--- src/bresenham.c
+++ src/bresenham.c
@@ -6,7 +6,7 @@
 /*   By: jsuonper <jsuonper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/02 17:44:59 by jsuonper          #+#    #+#             */
-/*   Updated: 2020/03/12 10:41:46 by jsuonper         ###   ########.fr       */
+/*   Updated: 2020/03/12 11:21:22 by jsuonper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,35 +28,38 @@ t_helpers			*get_helpers(t_3d_coords *p0, t_3d_coords *p1)
 	return (helpers);
 }
 
+void				free_line_struct(t_line_data *l)
+{
+	free(l->coords);
+	free(l->helpers);
+	free(l);
+}
+
 void				draw_line(t_3d_coords *p0, t_3d_coords *p1,
 t_mlx_struct *param)
 {
-	t_line_data		*line_struct;
+	t_line_data		*l;
 
-	line_struct = create_line_struct();
-	line_struct->coords = create_coords(p0->x, p0->y, p1->x, p1->y);
-	line_struct->helpers = get_helpers(p0, p1);
+	l = create_line_struct();
+	l->coords = create_coords(p0->x, p0->y, p1->x, p1->y);
+	l->helpers = get_helpers(p0, p1);
 	while (1)
 	{
 		mlx_pixel_put(param->mlx_ptr, param->win_ptr,
-			line_struct->coords->x0 + WIN_W / 2,
-			line_struct->coords->y0 + WIN_H / 2, 0xFFFFF);
-		if (line_struct->coords->x0 == line_struct->coords->x1 &&
-			line_struct->coords->y0 == line_struct->coords->y1)
+			l->coords->x0 + WIN_W / 2, l->coords->y0 + WIN_H / 2, 0xFFFFF);
+		if (l->coords->x0 == l->coords->x1 && l->coords->y0 == l->coords->y1)
 			break ;
-		line_struct->e2 = line_struct->helpers->err;
-		if (line_struct->e2 > -line_struct->helpers->dx)
+		l->e2 = l->helpers->err;
+		if (l->e2 > -l->helpers->dx)
 		{
-			line_struct->helpers->err -= line_struct->helpers->dy;
-			line_struct->coords->x0 += line_struct->helpers->sx;
+			l->helpers->err -= l->helpers->dy;
+			l->coords->x0 += l->helpers->sx;
 		}
-		if (line_struct->e2 < line_struct->helpers->dy)
+		if (l->e2 < l->helpers->dy)
 		{
-			line_struct->helpers->err += line_struct->helpers->dx;
-			line_struct->coords->y0 += line_struct->helpers->sy;
+			l->helpers->err += l->helpers->dx;
+			l->coords->y0 += l->helpers->sy;
 		}
 	}
-	free(line_struct->coords);
-	free(line_struct->helpers);
-	free(line_struct);
+	free_line_struct(l);
 }
